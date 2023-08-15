ab446cc7851a5dc1240901b44088b78c6dd805c7
Seong-Hoon-Lim <hooney200@gmail.com>
2023-08-15 11:25:32 +0900

    #16 - 게시글 view 테스트 내용 업데이트
    기존 테스트를 실패한 채 올리면 `gradle build` 가 실패하여 배포 자동화에 악영향을 미치는 경우가 있으므로,
    실패 테스트는 테스트 제외 처리하는 방향으로 정책을 수정
    이에 기능 구현 중인 유닛 테스트를 모두 `@Disabled` 처리
    
    그 밖에 추가로 생각난 테스트 방법을 추가 작성

--- fastcampus-board-project/src/test/java/com/fastcampus/boardproject/controller/ArticleControllerTest.java
+++ fastcampus-board-project/src/test/java/com/fastcampus/boardproject/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.boardproject.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
+    @Disabled("개발 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     void givenNoting_whenRequestingArticles_thenReturnsArticlesView() throws Exception {
@@ -32,10 +34,12 @@ class ArticleControllerTest {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.view().name("articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
 
     }
 
+    @Disabled("개발 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     void givenNoting_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
@@ -46,10 +50,13 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/2"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("article"));
+                .andExpect(MockMvcResultMatchers.view().name("articles/detail"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("article"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"));
 
     }
 
+    @Disabled("개발 중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     void givenNoting_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
@@ -59,10 +66,12 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articles/search"));
 
     }
 
+    @Disabled("개발 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
     void givenNoting_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
@@ -72,7 +81,8 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search=hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML));
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.TEXT_HTML))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articles/search-hashtag"));;
 
     }
 
