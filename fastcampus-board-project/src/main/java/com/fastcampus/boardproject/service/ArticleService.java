e1b1e9c797cc826f50aead543b544d4a15878dad
Seong-Hoon-Lim <hooney200@gmail.com>
2023-08-19 22:18:52 +0900

    #17 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    ArticleUpdateDto 를 ArticleWithCommentsDto 로 재설계
    엔티티와 dto 객체 간의 매핑 및 변환을 위한 static 메소드 추가
    response 패키지를 dto 패키지 내부에 추가
    dto 재설계에 따른 레포지토리 및 서비스 로직 코드 수정 및 테스트 코드 수정

--- fastcampus-board-project/src/main/java/com/fastcampus/boardproject/service/ArticleService.java
+++ fastcampus-board-project/src/main/java/com/fastcampus/boardproject/service/ArticleService.java
@@ -2,39 +2,37 @@ package com.fastcampus.boardproject.service;
 
 import com.fastcampus.boardproject.domain.type.SearchType;
 import com.fastcampus.boardproject.dto.ArticleDto;
-import com.fastcampus.boardproject.dto.ArticleUpdateDto;
+import com.fastcampus.boardproject.dto.ArticleWithCommentsDto;
 import com.fastcampus.boardproject.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class ArticleService {
-
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
     }
+
 }
