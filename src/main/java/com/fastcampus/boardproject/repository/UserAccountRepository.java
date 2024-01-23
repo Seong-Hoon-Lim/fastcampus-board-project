0d356134592f957279570c7c399ab900298d0999
Seong-Hoon-Lim <hooney200@gmail.com>
2024-01-23 22:29:17 +0900

    #93 - UserAccount api 노출 추가 + data rest projection 을 이용한 데이터 노출 필드 설정
    `UserAccount`를 노출할 api로 추가해 줌.
    이렇게 하면 다른 게시글, 댓글 등의 data rest api 노출 데이터 형식이
    원하지 않는 스타일로 바뀌게 된다.
    이를 수동으로 설정하기 위해 projection 기능을 사용하여
    노출할 필드들을 직접 지정함.

--- src/main/java/com/fastcampus/boardproject/repository/UserAccountRepository.java
+++ src/main/java/com/fastcampus/boardproject/repository/UserAccountRepository.java
@@ -1,8 +1,11 @@
 package com.fastcampus.boardproject.repository;
 
 import com.fastcampus.boardproject.domain.UserAccount;
+import com.fastcampus.boardproject.domain.projection.UserAccountProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(excerptProjection = UserAccountProjection.class)
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 
 }
